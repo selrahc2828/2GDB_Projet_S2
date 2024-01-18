6094853903de4384f2595dd919244628ca692999
MrMixio <rharlegand@gmail.com>
2024-01-18 12:40:32 +0100

    Jauge Fonctionne

--- Assets/S_Script/S_JaugeScript.cs
+++ Assets/S_Script/S_JaugeScript.cs
@@ -6,13 +6,26 @@ using UnityEngine.UI;
 public class S_JaugeScript : MonoBehaviour
 {
     [Header("Jauge")]
-    [SerializeField] public int m_JaugeLevel;
+    [SerializeField] public float m_JaugeLevel;
+    [SerializeField] private float m_MaxJaugeLevel;
+
+
+    [Header("Paramètres de la jauge")]
+    [SerializeField] private float m_JaugeDecreaseRate = 1f;
+    [SerializeField] private float m_JaugeIncreaseAmount = 1f;
 
 
     [Header("DebugHelper")]
     [SerializeField] public Text m_JaugeLevelText;
+    [SerializeField] private Image m_JaugeFillImage;
 
 
+    private void Start()
+    {
+        m_JaugeLevel = m_MaxJaugeLevel;
+    }
+
+    // Fonction qui permet de choper les cubes 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Cube"))
@@ -21,10 +34,25 @@ public class S_JaugeScript : MonoBehaviour
             Destroy(other.gameObject);
 
             // Augementer / Diminuer la Jauge a chaque cube rencontré, pour changer s'il il augemente ou baisse il suffi de changer les signes avec le m_JaugeLevel
-            m_JaugeLevel++;
+            m_JaugeLevel += m_JaugeIncreaseAmount;
 
-            // Observer le changement en text
-            m_JaugeLevelText.text = " JaugeLevel : " + m_JaugeLevel;
         }
     }
+
+    private void Update()
+    {
+        // appelle de la fonction qui diminue la jauge avec le temps
+        DecreaseJaugeOverTime();
+    }
+
+    private void DecreaseJaugeOverTime()
+    {
+        // Diminuer la jauge avec le temps
+        m_JaugeLevel -= m_JaugeDecreaseRate * Time.deltaTime;
+
+        // jauge ne descend pas en dessous de zéro
+        m_JaugeLevel = Mathf.Clamp(m_JaugeLevel, 0, m_MaxJaugeLevel);
+
+    }
+
 }
