8126fee24c7cc1dccff75fa3586a53e8977e25d5
MrMixio <rharlegand@gmail.com>
2024-01-24 12:20:22 +0100

    Scène added and Ui Added

--- Assets/S_Script/jauge.cs
+++ Assets/S_Script/jauge.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class jauge : MonoBehaviour
 {
-    private float originalScale;
+
+    [Header("Reference")]
+    [SerializeField] public Slider m_JaugeSlider;
+    
+   
     private float fullJauge;
     public float actualJauge;
     public float _time;
     // Start is called before the first frame update
     void Start()
     {
-        _time = 3;
-        originalScale = transform.localScale.y;
+        
         fullJauge = 100;
         actualJauge = fullJauge;
     }
@@ -20,10 +24,21 @@ public class jauge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        DecreaseJauge();
+        UpdateSliderUI();
+            
+    }
+
+    public void DecreaseJauge()
+    {
         if (actualJauge >= 0)
         {
-            actualJauge -= Time.deltaTime * 100/_time;
-            transform.localScale = new Vector3(transform.localScale.x, originalScale * (actualJauge / 100), transform.localScale.z);
-        }        
+            actualJauge -= Time.deltaTime * 100 / _time;
+        }
+    }
+
+    public void UpdateSliderUI()
+    {
+        m_JaugeSlider.value = 100 - actualJauge;
     }
 }
