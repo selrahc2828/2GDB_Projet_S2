7155048ea708a3bf261670dd16a37bdfb04bcb7f
selrahc2828 <charles.panet@orange.fr>
2024-01-24 11:27:59 +0100

    Ajout d'une zone qui fais baisser la jauge

--- Assets/S_Script/jauge.cs
+++ Assets/S_Script/jauge.cs
@@ -7,10 +7,11 @@ public class jauge : MonoBehaviour
     private float originalScale;
     private float fullJauge;
     public float actualJauge;
-
+    public float _time;
     // Start is called before the first frame update
     void Start()
     {
+        _time = 3;
         originalScale = transform.localScale.y;
         fullJauge = 100;
         actualJauge = fullJauge;
@@ -21,7 +22,7 @@ public class jauge : MonoBehaviour
     {
         if (actualJauge >= 0)
         {
-            actualJauge -= Time.deltaTime * 30;
+            //actualJauge -= Time.deltaTime * 100/_time;
             transform.localScale = new Vector3(transform.localScale.x, originalScale * (actualJauge / 100), transform.localScale.z);
         }        
     }
