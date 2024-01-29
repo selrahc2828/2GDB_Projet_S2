50fd4fef9f32818dcebb12f020217ef645d618e6
selrahc2828 <charles.panet@orange.fr>
2024-01-29 14:42:15 +0100

    création d'une scene pour une variante et début d'une autre variante

--- Assets/S_Script/Collectable.cs
+++ Assets/S_Script/Collectable.cs
@@ -4,18 +4,52 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    public jauge jaugeScript;
+    public jauge _jaugeScript;
+    public int _collectibleType = 0;
+
+    private int _smallValue = 5;
+    private int _mediumValue = 15;
+    private int _largeValue = 50;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (jaugeScript.actualJauge + 30 <= 100)
+            switch (_collectibleType)
             {
-                jaugeScript.actualJauge += 30;
-            }
-            else
-            {
-                jaugeScript.actualJauge = 100;
+                case 1:
+                    if (_jaugeScript.actualJauge + _smallValue <= 100)
+                    {
+                        _jaugeScript.actualJauge += _smallValue;
+                    }
+                    else
+                    {
+                        _jaugeScript.actualJauge = 100;
+                    }
+                    break;
+                case 2:
+                    if (_jaugeScript.actualJauge + _mediumValue <= 100)
+                    {
+                        _jaugeScript.actualJauge += _mediumValue;
+                    }
+                    else
+                    {
+                        _jaugeScript.actualJauge = 100;
+                    }
+                    break;
+                case 3:
+                    if (_jaugeScript.actualJauge + _largeValue <= 100)
+                    {
+                        _jaugeScript.actualJauge += _largeValue;
+                    }
+                    else
+                    {
+                        _jaugeScript.actualJauge = 100;
+                    }
+                    break;
+                default:
+                    Debug.Log("Invalid number.");
+                    break;
             }
         }
     }
