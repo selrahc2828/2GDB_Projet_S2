6382113872fcb5a879d4f60052caf827e37d0f79
MrMixio <rharlegand@gmail.com>
2024-01-30 15:14:59 +0100

    Cube Spawner working

--- Assets/S_Script/LifeAndDeath/SpawnerControl.cs
+++ Assets/S_Script/LifeAndDeath/SpawnerControl.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class SpawnerControl : MonoBehaviour
 {
-    public GameObject prefabToSpawn; // Référence au prefab à faire spawn
-    public float spawnInterval = 2f; // Intervalle de spawn en secondes
+    public GameObject prefabToSpawn; 
+    public float spawnInterval = 2f; 
 
-    // Start is called before the first frame update
+   
     void Start()
     {
-        // Commencer à faire spawn le prefab à l'intervalle spécifié
         InvokeRepeating("SpawnPrefab", 0f, spawnInterval);
     }
 
-    // Méthode pour faire spawn le prefab
+   
     void SpawnPrefab()
     {
-        // Faire spawn le prefab à la position du spawner avec l'identité de rotation
         Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
     }
 }
