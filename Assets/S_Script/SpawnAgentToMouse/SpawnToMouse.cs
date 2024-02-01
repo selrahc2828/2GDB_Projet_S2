c34441a09f6f0385d4bfd1717a091d77070f8048
MrMixio <rharlegand@gmail.com>
2024-02-01 12:45:29 +0100

    Code Comment (Script is on the Objet "Surface Navmesh"

--- Assets/S_Script/SpawnAgentToMouse/SpawnToMouse.cs
+++ Assets/S_Script/SpawnAgentToMouse/SpawnToMouse.cs
@@ -12,8 +12,8 @@ public class SpawnToMouse : MonoBehaviour
 
 
     [Header("Parametter")]
-    public float spawnInterval = 0.5f; 
-    public float randomOffsetRange = 1.0f; 
+    public float _spawnInterval = 0.5f; 
+    public float _randomOffsetRange = 1.0f; 
     public float _AgentSpawnCount = 0;
 
     private float spawnTimer;
@@ -21,30 +21,40 @@ public class SpawnToMouse : MonoBehaviour
 
     private void Update()
     {
-
+        // While the input is press do the thing in the IF 
         if (Input.GetMouseButton(0))
         {
+            // Timer to limit de number of spawn, here we juste add a sec to DeltaTime
             spawnTimer -= Time.deltaTime;
 
+            // While SpawnTimer < _AgentSpawnCount, that spawn more thant 1 agent on the time that spawnTimer is below the _AgentSpawnCount,, Reduc the value _AgentSpawnCount to have less spawn in sec 
             while (spawnTimer < _AgentSpawnCount)
             {
-                spawnTimer = spawnInterval;
+                // Update the timer
+                spawnTimer = _spawnInterval;
+                // Call the fonction that spawn agent 
                 SpawnAgent();
             }
 
         }
     }
 
+    // Fonction that spawn the agent 
     void SpawnAgent()
     {
+        // Raycast Shoot camera to the layer floor where the mouse is 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
+        // if the raycast hit the layer floor that call the fonction In 
         if (Physics.Raycast(ray, out hit, float.MaxValue, _affectedLayer))
         {
-            Vector3 spawnPosition = hit.point + Random.insideUnitSphere * randomOffsetRange;
+            // Set the position with a little offset to be more organic
+            Vector3 spawnPosition = hit.point + Random.insideUnitSphere * _randomOffsetRange;
 
+            // Create new agent at the mouse position with the offset
             GameObject newAgent = Instantiate(_AgentPrefab, spawnPosition, Quaternion.identity);
+            // Add the Spawned agent to the list (can be usefull for furture)
             spawnedAgents.Add(newAgent);
 
         }
