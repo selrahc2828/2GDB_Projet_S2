2a093c782fe1588f115ca38181256058fbc5e52c
MrMixio <rharlegand@gmail.com>
2024-02-05 19:33:28 +0100

    Added changement Script

---
+++ Assets/S_Script/ScripSceneChang.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptSceneChang : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangeScene(0); 
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangeScene(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ChangeScene(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            ChangeScene(3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            ChangeScene(4);
+        }
+    }
+
+    void ChangeScene(int sceneIndex)
+    {
+        if (sceneIndex >= 0 && sceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("Index de scène invalide.");
+        }
+    }
+}
