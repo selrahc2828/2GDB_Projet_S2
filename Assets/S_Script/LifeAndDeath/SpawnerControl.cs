80ccd14f1f4a4b091fc74d3cb3ac40720172324f
MrMixio <rharlegand@gmail.com>
2024-02-07 17:44:18 +0100

    Jauge Working

--- Assets/S_Script/LifeAndDeath/SpawnerControl.cs
+++ Assets/S_Script/LifeAndDeath/SpawnerControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpawnerControl : MonoBehaviour
 {
+    public JaugeScript _JaugeScript;
     public GameObject prefabToSpawn; 
     public float spawnInterval = 2f; 
 
@@ -13,7 +14,7 @@ public class SpawnerControl : MonoBehaviour
         InvokeRepeating("SpawnPrefab", 0f, spawnInterval);
     }
 
-   
+    
     void SpawnPrefab()
     {
         Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
