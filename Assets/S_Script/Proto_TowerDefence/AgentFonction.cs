9f00655f2dcae7012a68624d86b3c1db09547782
MrMixio <rharlegand@gmail.com>
2024-02-16 11:44:56 +0100

    SlowEnemyWorking

--- Assets/S_Script/Proto_TowerDefence/AgentFonction.cs
+++ Assets/S_Script/Proto_TowerDefence/AgentFonction.cs
@@ -1,22 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AgentFonction : MonoBehaviour
 {
-
     [Header("Choise Comportement")]
     public bool _ShootEnemy;
     public bool _SlowEnemy;
 
-    void Start()
+    [Header("Slow Parameters")]
+    public float _slowdownSpeed;
+    public float _InitialSpeed;
+
+
+    public SphereCollider _slowSphereCollider;
+
+
+    private void Update()
     {
-        
+        ShootToEnemy();
     }
 
-    
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+  
+        if (_SlowEnemy == true && other.CompareTag("AgentMechant"))
+        {
+            NavMeshAgent enemyAgent = other.GetComponent<NavMeshAgent>();
+
+            if (enemyAgent != null)
+            {
+                enemyAgent.speed = _slowdownSpeed;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+     
+        if (!_SlowEnemy|| other.CompareTag("AgentMechant"))
+        {
+     
+            NavMeshAgent enemyAgent = other.GetComponent<NavMeshAgent>();
+
+      
+            if (enemyAgent != null)
+            {
+                enemyAgent.speed = _InitialSpeed;
+            }
+        }
+    }
+
+    public void ShootToEnemy()
+    {
+
+
     }
 }
