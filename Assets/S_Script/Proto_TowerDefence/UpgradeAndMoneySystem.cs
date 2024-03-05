242d2dd22c8baddf9ccca28da371f9db0f5d791b
MrMixio <rharlegand@gmail.com>
2024-03-05 15:45:31 +0100

    Update fireRate System

--- Assets/S_Script/Proto_TowerDefence/UpgradeAndMoneySystem.cs
+++ Assets/S_Script/Proto_TowerDefence/UpgradeAndMoneySystem.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeAndMoneySystem : MonoBehaviour
 {
     public int _moneyNumber;
 
-    private void Start()
+    [Header("Reference")]
+    [SerializeField] private List<AgentFonction> _allAgents;
+    public Text _ActualDamage;
+    public Text _FireRate;
+    public Text _Range;
+
+    [Header("Limit")]
+    public int _MaxDamageValue;
+    public int _MaxFireRateValue;
+    public int _MaxRangeValue;
+    public float _MinValue;
+
+    void Start()
     {
         _moneyNumber = 0;
+        _allAgents = new List<AgentFonction>(FindObjectsOfType<AgentFonction>());
     }
-    // Update is called once per frame
-    void Update()
+
+    #region ShootDamage
+    public void UpgradeShootDamage1()
     {
-        
+        foreach (AgentFonction agent in _allAgents)
+        {
+            agent._damageAmount = Mathf.Clamp(agent._damageAmount + 5, 0, _MaxDamageValue);
+            _ActualDamage.text = "Actual Damage = " + agent._damageAmount;
+        }
     }
+    #endregion
+
+
+    #region ShootRange
+    public void UpgradeShootRange1()
+    {
+        foreach (AgentFonction agent in _allAgents)
+        {
+            agent._ShootRange = Mathf.Clamp(agent._ShootRange + 5, 0, _MaxRangeValue);
+            _Range.text = "Actual Range = " + agent._ShootRange;
+        }
+    }
+    #endregion
+
+
+
+    #region ShootCadence
+    public void UpgradeShootFireRate1()
+    {
+        foreach (AgentFonction agent in _allAgents)
+        {
+            agent._fireRate = Mathf.Clamp(agent._fireRate - 0.5f, _MinValue, _MaxFireRateValue);
+            _FireRate.text = "Actual FireRate = " + agent._fireRate;
+        }
+    }
+    #endregion
+
+
+   
 }
