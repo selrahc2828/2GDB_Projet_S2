8052c80b2ad11718846024b31b89997a8d48a045
MrMixio <rharlegand@gmail.com>
2024-03-16 11:01:24 +0100

    Changement de camera

--- Assets/S_Script/Proto_TowerDefence/CameraRotation.cs
+++ Assets/S_Script/Proto_TowerDefence/CameraRotation.cs
@@ -16,11 +16,6 @@ public class CameraRotation : MonoBehaviour
     [SerializeField] public Vector2 pitchMinMax = new Vector2(-40, 85);
 
 
-    [Header("Zoom")]
-    [SerializeField] public float zoomSpeed;
-    [SerializeField] public float minZoomDistance;
-    [SerializeField] public float maxZoomDistance;
-
     Vector3 currentRotation;
 
     private void LateUpdate()
@@ -29,12 +24,6 @@ public class CameraRotation : MonoBehaviour
         {
             RotateCamera();
         }
-
-        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
-        distanceFromTarget = Mathf.Clamp(distanceFromTarget - scrollInput * zoomSpeed, minZoomDistance, maxZoomDistance);
-
-        Vector3 desiredPosition = target.position - transform.forward * distanceFromTarget;
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, rotationSmoothTime);
     }
 
     void RotateCamera()
