dbd70b4287d8a1fd29e4f42f5c9d630e09dd2856
MrMixio <rharlegand@gmail.com>
2024-04-05 11:15:09 +0200

    Changement d'intensity - Debut du feedback fatigue

--- Assets/S_Script/Proto_TowerDefence/RecognizeItsSelf.cs
+++ Assets/S_Script/Proto_TowerDefence/RecognizeItsSelf.cs
@@ -1,7 +1,7 @@
 using FMOD;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
+//using System.Drawing;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
@@ -16,8 +16,8 @@ public class RecognizeItsSelf : MonoBehaviour
     public float _exaustionLevel;
     private float _exaustionTrueLevel;
     private float _exaustionMaxLevel;
-    public Material _initialMaterial;
-    public Material _exaustedMaterial;
+    public float _Intensity;
+    public Material _ShaderMaterial;
     public MeshRenderer _meshRenderer;
 
 
@@ -40,7 +40,6 @@ public class RecognizeItsSelf : MonoBehaviour
 
     private void Awake()
     {
-        _meshRenderer = GetComponent<MeshRenderer>();
         _gameManager = GameObject.FindObjectOfType<GameManager>();
         _TraceScript = GameObject.FindObjectOfType<AgentToTrace>();
     }
@@ -139,12 +138,26 @@ public class RecognizeItsSelf : MonoBehaviour
 
     public void UpdateExaustionMeter()
     {
-        // Lerp between color1 and color2 based on lerpAmount
-        UnityEngine.Color lerpedColor = UnityEngine.Color.Lerp(_initialMaterial.color, _exaustedMaterial.color, _exaustionLevel);
+        
+        Color initialColor = new Color(0f, 123f / 255f, 191f / 255f);
 
-        // Apply the lerped color to the renderer's material
-        _meshRenderer.material.color = lerpedColor;
+        
+        Color fatigueColor = new Color(0f, 15f / 255f, 24f / 255f);
+
+        // Définir une couleur finale en interpolant entre la couleur initiale et la couleur de fatigue basée sur le niveau d'épuisement
+        Color finalColor = Color.Lerp(initialColor, fatigueColor, _exaustionLevel);
+
+        // Définir une intensité pour la couleur finale
+        _Intensity = 4.5f + (_exaustionLevel * 10f); // Ajustez cette formule selon vos préférences pour l'intensité
+
+       
+        finalColor *= _Intensity;
+
+        // Appliquer la couleur calculée à la propriété FresnelColor du material
+        _meshRenderer.material.SetColor("_FresnelColor", finalColor);
     }
+
+
 
     public List<NavMeshAgent> WitchListIsIt()
     {
