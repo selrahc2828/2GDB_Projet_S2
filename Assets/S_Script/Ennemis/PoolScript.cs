970e02f761e11f8808be6c930605f3e798081e15
MrMixio <rharlegand@gmail.com>
2024-05-24 15:38:11 +0200

    Feedback Tour pas correct

--- Assets/S_Script/Ennemis/PoolScript.cs
+++ Assets/S_Script/Ennemis/PoolScript.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 public class PoolScript : MonoBehaviour
 {
     public bool _infected;
+
+    [Header("ColorIfInfected")]
+    [ColorUsage(false, true)]
+    public Color _InitialColor;
+    [ColorUsage(false, true)]
+    public Color _InfectedColor;
+
+    public MeshRenderer _meshRenderer;
+    public MeshRenderer _meshRenderer2;
+    
+    private bool _isTransitioning = false;
+    private bool _previousInfectedState = false;
+
+
     public void CheckSurrounding()
     {
         _infected = false;
-        Collider[] _enemysInZone = Physics.OverlapSphere(transform.position, 10);
-        foreach (Collider enemy in _enemysInZone)
+        Collider[] _enemiesInZone = Physics.OverlapSphere(transform.position, 10);
+        foreach (Collider enemy in _enemiesInZone)
         {
             if (enemy.CompareTag("AgentMechant") && enemy.GetComponent<BuzzKiller>() != null)
             {
-                _infected = true; 
+                _infected = true;
             }
-        }
-        if (_infected )
-        {
-            gameObject.tag = "Infected";
-        }
-        else
-        {
-            gameObject.tag = "Untagged";
         }
     }
 
@@ -30,4 +36,46 @@ public class PoolScript : MonoBehaviour
     {
         CheckSurrounding();
     }
+
+    private void Update()
+    {
+        LerpOnInfectedColor();
+    }
+
+    private void LerpOnInfectedColor()
+    {
+        if (_infected != _previousInfectedState)
+        {
+            StartCoroutine(StartColorTransition());
+            _previousInfectedState = _infected;
+        }
+    }
+
+    private IEnumerator StartColorTransition()
+    {
+        _isTransitioning = true;
+
+        float transitionDuration = 1.0f;
+        float startTime = Time.time;
+        float elapsedTime = 0;
+
+        while (elapsedTime < transitionDuration)
+        {
+            elapsedTime = Time.time - startTime;
+
+            float lerpFactor = _infected ? Mathf.Clamp01(elapsedTime / transitionDuration) : Mathf.Clamp01((transitionDuration - elapsedTime) / transitionDuration);
+            Color lerpedColor = Color.Lerp(_InitialColor, _InfectedColor, lerpFactor);
+
+            _meshRenderer.material.SetColor("_EmissionColor", lerpedColor);
+            _meshRenderer.materials[1].SetColor("_Color", lerpedColor);
+
+            _meshRenderer2.material.SetColor("_EmissionColor", lerpedColor);
+            _meshRenderer2.materials[1].SetColor("_Color", lerpedColor);
+
+            yield return null;
+        }
+
+        _isTransitioning = false;
+    }
+
 }
