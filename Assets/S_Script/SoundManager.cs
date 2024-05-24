69902c70d80c63eb456acdfbd411f2319abc093e
EnemyStand <ivan.aranda2310@gmail.com>
2024-05-24 14:54:57 +0200

    Updated music code (sucks hard mute game)

--- Assets/S_Script/SoundManager.cs
+++ Assets/S_Script/SoundManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Pool;
 
@@ -15,16 +16,16 @@ public class SoundManager : MonoBehaviour
     
     
     //Internal variables
-    public float engagement;
     public float hitPoints;
-    public float pooled;
     public float homewrecker;
     public float buzzkiller;
-    public float enemyQuanity;
+    public int songstate = 0;
     
     //Fmod parameters
     private static FMOD.Studio.EventInstance levelMusic;
+    
 
+    
     void Start()
     {
         //init music
@@ -33,20 +34,11 @@ public class SoundManager : MonoBehaviour
         levelMusic.release();
     }
     void Update()
-    {
-        // Agents engaged
-
-        engagement = 1 - activeAgentInformation._proportionOfAgentAssigned;
-        
+    { 
         // Hit Points
-        
         hitPoints = towerLifeScript.healthPercentageVariable;
         
-        // Pooled Agents
-        pooled = 0f;
-        
         // homewrecker
-
         if (gameManager._numberOfHomeWreckerOnScreen == 0)
         {
             homewrecker = 0f;
@@ -57,7 +49,6 @@ public class SoundManager : MonoBehaviour
         }
         
         //buzzkiller
-
         if (gameManager._numberOfBuzzKillerOnScreen == 0)
         {
             buzzkiller = 0f;
@@ -67,26 +58,12 @@ public class SoundManager : MonoBehaviour
             buzzkiller = 1f;
         }
         
-        //distance to tower
-
-        if (gameManager._numberOfBaseEnemyOnScreen == 0)
-        {
-            enemyQuanity = 0f;
-        }
-        else
-        {
-            enemyQuanity = 1f;
-        }
-        
-        
-        levelMusic.setParameterByName("Engagement", engagement);
+        //manage transitions
+            
         levelMusic.setParameterByName("Hit Points", hitPoints);
-        levelMusic.setParameterByName("Pooled", pooled);
         levelMusic.setParameterByName("Homewrecker", homewrecker);
         levelMusic.setParameterByName("Buzzkiller", buzzkiller);
-        levelMusic.setParameterByName("EnemyQuantity", enemyQuanity);
-        
-
+        levelMusic.setParameterByName("Songstate", songstate);
 
     }
 }
